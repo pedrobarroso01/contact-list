14a72f3ddb242a9224cb227c409db313929eba96
pedrobarroso01 <pedrobarrosocontato@gmail.com>
2024-03-06 18:04:54 -0300

    Button visibility

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { useState } from 'react';
 import Formulario from './Form.js';
 import Tabela from './Tabela.js';
 
 function App() {
+
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+
+
   return (
     <div class = 'content'>
-      <Formulario/>
+      <Formulario botao={btnCadastrar} />
       <Tabela/>
     </div>
   );
--- front-end/src/Form.js
+++ front-end/src/Form.js
@@ -1,13 +1,21 @@
-function Formulario() {
+function Formulario({botao}) {
     return (
         <form>
             <input type="text" placeholder="First name" className="form-control"/>
             <input type="number" placeholder="Last name" className="form-control"/>
             <input type="number" placeholder="Phone" className="form-control" />
-            <input type="button" value="cadastrar" className="btn btn-primary"/>
-            <input type="button" value="alterar" className="btn btn-warning"/>
-            <input type="button" value="excluir" className="btn btn-danger"/>
-            <input type="button" value="cancelar" className="btn btn-secondary"/>
+
+            {
+                botao
+                ?
+                <input type="button" value="Cadastrar" className="btn btn-primary"/>
+                :
+                <div>
+                    <input type="button" value="Alterar" className="btn btn-warning"/>
+                    <input type="button" value="Excluir" className="btn btn-danger"/>
+                    <input type="button" value="Cancelar" className="btn btn-secondary"/>
+                </div>
+            }
 
         </form>
     )
